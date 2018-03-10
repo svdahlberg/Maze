13d6eda614cc2f031656beee3d91d464d1bd8a08
Svante <svante.dahlberg@gmail.com>
2018-03-10 13:02:56 +0100

    chage button animation

--- Maze/ButtonNode.swift
+++ Maze/ButtonNode.swift
@@ -12,14 +12,28 @@ class ButtonNode: SKSpriteNode {
     
     var action: (() -> ())?
     
-    var isHighlighted = false {
+    private lazy var scaleDownAction: SKAction = {
+        let action = SKAction.scale(by: 0.9, duration: 0.1)
+        action.timingMode = .easeOut
+        return action
+    }()
+    
+    private lazy var scaleUpAction: SKAction = {
+        let action = SKAction.scale(by: 1.1, duration: 0.1)
+        action.timingMode = .easeIn
+        return action
+    }()
+    
+    private let fadeOutAction = SKAction.fadeAlpha(to: 0.7, duration: 0.1)
+    
+    private let fadeInAction = SKAction.fadeAlpha(to: 1, duration: 0.1)
+    
+    private(set) var isHighlighted = false {
         didSet {
             guard oldValue != isHighlighted else { return }
-            let newScale: CGFloat = isHighlighted ? 0.9 : 1.1
-            let scaleAction = SKAction.scale(by: newScale, duration: 0.1)
-            let newColorBlendFactor: CGFloat = isHighlighted ? 1.0 : 0.0
-            let colorBlendAction = SKAction.colorize(withColorBlendFactor: newColorBlendFactor, duration: 0.1)
-            run(SKAction.group([scaleAction, colorBlendAction]))
+            let scaleAction = isHighlighted ? scaleDownAction : scaleUpAction
+            let fadeAction  = isHighlighted ? fadeOutAction : fadeInAction
+            run(SKAction.group([scaleAction, fadeAction]))
         }
     }
     
@@ -45,15 +59,12 @@ class ButtonNode: SKSpriteNode {
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
         isHighlighted = false
-        if contains(touches: touches) {
-            action?()
-        }
+        if contains(touches: touches) { action?() }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesCancelled(touches, with: event)
         isHighlighted = false
-        
     }
     
     private func contains(touches: Set<UITouch>) -> Bool {
@@ -67,6 +78,3 @@ class ButtonNode: SKSpriteNode {
     }
   
 }
-
-
-
